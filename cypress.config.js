ce83cb09b8d22fbfb3f904ab328c36a592cafbb6
Hybecorp <160170666+0fficialmanagement@users.noreply.github.com>
2025-06-30 06:12:28 +0000

    go

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'cypress';
-
-export default defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:3000', // Change if your dev server runs on a different port
-    specPattern: 'cypress/e2e/**/*.cy.js',
-    supportFile: false,
-  },
-});
